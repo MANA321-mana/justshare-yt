03eda6d380500faecadb89b28244ec6a76c07a15
someone <someone@someplace.com>
2022-05-09 18:59:07 +0530

    initial

---
+++ config/db.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+function connectDB(){
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, {   
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    const connection = mongoose.connection;
+    console.log('1');
+    connection.once('open', function () {
+        console.log('MongoDB running');
+      }).on('error', function (err) {
+        console.log(err);
+      });
+}
+
+module.exports = connectDB;
